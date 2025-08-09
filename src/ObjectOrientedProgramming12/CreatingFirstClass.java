df1056cb337a0f783ba9659eb7c3da29dc4c5c39
Bryan Vela <bryanedinson1998@hotmail.com>
2025-08-08 19:00:30 -0500

    OOP 20%

--- src/ObjectOrientedProgramming12/CreatingFirstClass.java
+++ src/ObjectOrientedProgramming12/CreatingFirstClass.java
@@ -2,6 +2,37 @@ package ObjectOrientedProgramming12;
 
 public class CreatingFirstClass {
     public static void main(String[] args) {
-        
+        // Creating an Animal example class
+        class Animal {
+            // Attributes
+            String name;
+            int age;
+
+            // Constructor
+            Animal(String name, int age) {
+                this.name = name;
+                this.age = age;
+            }
+
+            // Method without args
+            void displayInfo() {
+                System.out.println("Animal Name: " + name + ", Age: " + age);
+            }
+
+            // Method with args
+            void makeSound(String sound) {
+                System.out.println(name + " makes a " + sound + " sound.");
+            }
+        }
+
+        // Creating objects
+        Animal animal1 = new Animal("Lion", 5);
+        Animal animal2 = new Animal("Elephant", 10);
+
+        // Accessing object methods
+        animal1.displayInfo();
+        animal1.makeSound("Roar");
+        animal2.displayInfo();
+        animal2.makeSound("Trumpet");
     }
 }
---
+++ src/ObjectOrientedProgramming12/MethodsWithArgsOverloadEncapsulation.java
@@ -0,0 +1,27 @@
+package ObjectOrientedProgramming12;
+
+public class MethodsWithArgsOverloadEncapsulation {
+    // To demonstrate method overloading and encapsulation, let's create a class called 'Calculator'
+    class Calculator {
+
+        // Overloading: Same method name with different parameters
+        // Method overloading for addition
+        int add(int a, int b) {
+            return a + b;
+        }
+        // Method overloading for addition with double parameters
+        double add(double a, double b) {
+            return a + b;
+        }
+
+        // Encapsulation: Hiding the implementation details
+        // Method with encapsulation
+        private int subtract(int a, int b) {
+            return a - b;
+        }
+        // Public method to access the private subtract method
+        public int publicSubtract(int a, int b) {
+            return subtract(a, b);
+        }
+    }
+}
