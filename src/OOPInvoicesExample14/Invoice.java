3b286e1d3e644b609b9615d6ed8307021a95519d
Bryan Vela <bryanedinson1998@hotmail.com>
2025-09-13 15:15:40 -0300

    Ex OPP 65%

--- src/OOPInvoicesExample14/Invoice.java
+++ src/OOPInvoicesExample14/Invoice.java
@@ -7,6 +7,21 @@ public class Invoice {
     private String description;
     private Date date;
     private Client client;
+    private ItemInvoice[] items;
+    private int indexItems;
+    public static final int MAX_ITEMS = 10; // defining a constant for maximum items
+    private static int lastFolio; // static variable to keep track of the last folio number
+
+    // Constructor
+
+    public Invoice(String description, Client client) {
+        this.description = description;
+        this.client = client;
+        this.items = new ItemInvoice[MAX_ITEMS]; // Assuming a maximum of 10 items per invoice
+        this.date = new Date(); // Set the invoice date to the current date
+        this.indexItems = 0; // Initialize the index for items
+        this.folio = ++lastFolio; // Auto-increment folio number
+    }
 
     public int getFolio() {
         return folio;
@@ -24,8 +39,8 @@ public class Invoice {
         return client;
     }
 
-    public void setFolio(int folio) {
-        this.folio = folio;
+    public ItemInvoice[] getItems() {
+        return items;
     }
 
     public void setDescription(String description) {
@@ -40,4 +55,10 @@ public class Invoice {
         this.client = client;
     }
 
+    // Function to add item invoices
+    public void addItemInvoice(ItemInvoice item) {
+        if (indexItems < MAX_ITEMS) {
+            this.items[indexItems++] = item;
+        }
+    }
 }
